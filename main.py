758766dbb15d5d08cfe933b2a2e4a5d3abb308f0
t4t5u0 <ymmtryk0902@gmail.com>
2021-02-17 03:48:37 +0900

    [fix] raw-stringを通常の文字列に

--- main.py
+++ main.py
@@ -8,25 +8,25 @@ import requests
 import tweepy
 
 # api-keyとかの準備
-## twitter
-consumer_key='CONSUMER_KEY'
-consumer_secret='CONSUMER_KEY_SECRET'
-access_token_key='ACCESS_TOKEN_KEY'
-access_token_secret='ACCESS_TOKEN_KEY_SECRET'
+# twitter
+consumer_key = 'CONSUMER_KEY'
+consumer_secret = 'CONSUMER_KEY_SECRET'
+access_token_key = 'ACCESS_TOKEN_KEY'
+access_token_secret = 'ACCESS_TOKEN_KEY_SECRET'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token_key, access_token_secret)
 
 api = tweepy.API(auth)
 
-## ApexLegends-tracker
+# ApexLegends-tracker
 url = "https://public-api.tracker.gg/v2/apex/standard/profile/{PLATFORM}/{ID}"
-header = {"TRN-Api-key":'TRN_API_KEY'}
+header = {"TRN-Api-key": 'TRN_API_KEY'}
 
 res = requests.get(url, headers=header).json()
 
 # 前回トータルキル数の読み込み
-f = open(R"last_kills.txt", 'r', encoding='UTF-8')
+f = open("last_kills.txt", 'r', encoding='UTF-8')
 
 last_kills = f.read()
 
@@ -40,10 +40,11 @@ today = datetime.today()
 yesterday = today - timedelta(days=1)
 
 # ツイート
-api.update_status("ApexLegends:" + datetime.strftime(yesterday, '%Y-%m-%d') + "のキル数は{}です。現在のトータルキル数は{}です。".format(int(now_kills)-int(last_kills), now_kills))
+api.update_status("ApexLegends:" + datetime.strftime(yesterday, '%Y-%m-%d') +
+                  "のキル数は{}です。現在のトータルキル数は{}です。".format(int(now_kills)-int(last_kills), now_kills))
 
 # last_kills.txtの更新
-f = open(R"last_kills.txt", 'w', encoding='UTF-8')
+f = open("last_kills.txt", 'w', encoding='UTF-8')
 
 f.write(str(now_kills))
 
